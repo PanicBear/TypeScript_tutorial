ee5eec264bb28e0a9326a10984b3b032989a1a39
ClarXo <imp_rove@naver.com>
2021-04-12 07:34:14 +0900

    Motion - DI with type constructor

--- motion/dist/app.js
+++ motion/dist/app.js
@@ -2,10 +2,10 @@ import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { VideoComponent } from "./components/page/item/video.js";
-import { PageComponent } from "./components/page/page.js";
+import { PageComponent, PageItemComponent } from "./components/page/page.js";
 class App {
     constructor(appRoot) {
-        this.page = new PageComponent();
+        this.page = new PageComponent(PageItemComponent);
         this.page.attachTo(appRoot);
         const image = new ImageComponent("Image Title", "https://picsum.photos/600/300");
         const video = new VideoComponent("YouTube video player", "https://www.youtube.com/watch?v=N2jgVT9chaQ&t=9s&ab_channel=%ED%95%98%ED%83%80");
--- motion/src/components/page/page.ts
+++ motion/src/components/page/page.ts
@@ -4,15 +4,31 @@ export interface Composable {
   addChild(child: Component): void;
 }
 
-class PageItemComponent
+type OnCloseListener = () => void;
+
+interface SectionContainer extends Component, Composable {
+  setOnCloseListener(listener: OnCloseListener): void;
+}
+
+// SectionContainer를 구현하는 모든 클래스 허용
+type SectionContainerConstructor = {
+  new (): SectionContainer; // 생성자 정의 : 생성자 호출 시 해당 타입을 생성하는 어떤 클래스라도 허용
+};
+
+export class PageItemComponent
   extends BaseComponent<HTMLElement>
-  implements Composable {
+  implements SectionContainer {
+  private closeListener?: OnCloseListener;
   constructor() {
     super(`<li class="page-item">
             <section class="page-item__body">
               <div class="page-item__controls"><button class="close">&times;</button></div>
             </section>
           </li>`);
+    const closeBtn = this.element.querySelector(".close")! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
   }
   addChild(child: Component) {
     const container = this.element.querySelector(
@@ -20,18 +36,23 @@ class PageItemComponent
     )! as HTMLElement;
     child.attachTo(container);
   }
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
+  }
 }
 
 export class PageComponent
   extends BaseComponent<HTMLUListElement>
   implements Composable {
-  constructor() {
+  constructor(private pageItemConstructor: SectionContainerConstructor) {
     super('<ul class="page"></ul>');
   }
-
   addChild(section: Component) {
-    const item = new PageItemComponent();
+    const item = new this.pageItemConstructor();
     item.addChild(section);
     item.attachTo(this.element, "beforeend");
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    });
   }
 }
